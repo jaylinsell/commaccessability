06fd1b579831d5dc965114213dce25738fbeee6b
Jay Linsell <holla@jaylinsell.com>
2021-12-21 22:32:37 +1100

    adding styles and starting markup

--- header.php
+++ header.php
@@ -25,35 +25,39 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'commaccessability' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
+	<header id="masthead" class="header">
+		<div class="header__branding">
+
+			<?php the_custom_logo(); ?>
+			<!-- <?php
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="header__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="header__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
-			$commaccessability_description = get_bloginfo( 'description', 'display' );
-			if ( $commaccessability_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $commaccessability_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?> -->
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'commaccessability' ); ?></button>
+		<section class="header__top">
+			<button class="btn btn--secondary" type="button" role="listbox">Accessability</button>
+			<button class="btn btn--secondary btn--icon" type="button" aria-label="Search">S</button>
+			<button class="btn btn--primary" type="button">Book Transport</button>
+		</section>
+
+		<nav id="site-navigation" class="nav">
+			<button class="nav__toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'commaccessability' ); ?></button>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+						'menu_class'		 => 'nav__list',
+					)
+				);
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
